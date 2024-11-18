02e10c6b32172a9bb1412f39aceeecfc07eed6e0
Efe Yilmaz <remziefeylmz@gmail.com>
2024-11-18 23:52:19 +0300

    Add project files.

---
+++ MvcStok/Controllers/KategoriController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORILER.ToList();
+            return View(degerler);
+        }
+
+        //---------------------------- kategori ekleme get ve post işlemleri -----------------------------
+        // HTTPGET işlemi bize sayfayı döndürür, httppost ise girilen verilerin kaydedilmesini sağlar. 
+        [HttpGet]
+        public ActionResult YeniKategori()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniKategori(TBLKATEGORILER p1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("YeniKategori");
+            }
+            db.TBLKATEGORILER.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("INDEX");
+        }
+
+        //------------------------- kategori silme işlemi ---------------------------------
+        public ActionResult SIL(int id) 
+        {
+            var kategori = db.TBLKATEGORILER.Find(id);
+            db.TBLKATEGORILER.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index","KATEGORI");
+        }
+        //-------------------- Kategorileri güncelleme sayfasına o id deki kategorinin bilgilerini getirme işlemi
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktgr = db.TBLKATEGORILER.Find(id);
+            return View("KategoriGetir",ktgr);
+        }
+
+        public ActionResult Guncelle(TBLKATEGORILER p1)
+        {
+            var ktg = db.TBLKATEGORILER.Find(p1.KATEGORIID);
+            ktg.KATEGORIAD = p1.KATEGORIAD; 
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
